460ce52b0175d7c076fd6774c04d719744101669
Kaye Wattawa <kayewattawa@Kayes-MacBook-Pro.local>
2016-05-08 11:37:44 -0600

    fix ordering of list items

--- checklist/views.py
+++ checklist/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 
 def Index(request):
-	items = Task.objects.all()
+	items = Task.objects.order_by('due_date')
 	return render(request, 'checklist/index.html', {'items': items})
